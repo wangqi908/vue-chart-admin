cdcc773299993b0f5579b6918f67927cbf3857a9
wangqi <wangqi908@hotmail.com>
2020-06-10 11:45:33 +0800

    用户表和直播数据插入接口完成

--- zb-chart-server/routes/users.js
+++ zb-chart-server/routes/users.js
@@ -9,10 +9,24 @@ router.get('/', (req, res) => {
 })
 
 router.post('/add', (req, res) => {
-  let Model = new UserModel(req.body)
-  Model.save((err, user) => {
+  UserModel.create(req.body, function (err, docs) {
     res.send({ status: 200, data: 'ok' })
   })
 })
 
+router.post('/insert', async (req, res) => {
+  let { list } = req.body
+  const allData = await UserModel.find({})
+
+  // 根据enterTime判断,防止重复插入
+  let newList = []
+  list.forEach(newItem => {
+    let flag = allData.find(oldItem => oldItem.enterTime === newItem.enterTime)
+    if (!flag) newList.push(newItem)
+  })
+
+  UserModel.insertMany(newList).then(data => {
+    res.send({ status: 200, data: 'ok' })
+  })
+})
 module.exports = router
--- zb-chart-server/routes/zb.js
+++ zb-chart-server/routes/zb.js
@@ -8,8 +8,23 @@ router.get('/', (req, res) => {
 })
 
 router.post('/add', (req, res) => {
-  let Model = new ZbModel(req.body)
-  Model.save((err, user) => {
+  ZbModel.create(req.body, function (err, docs) {
+    res.send({ status: 200, data: 'ok' })
+  })
+})
+
+router.post('/insert', async (req, res) => {
+  let { list } = req.body
+  const allData = await ZbModel.find({})
+
+  // 根据enterTime判断,防止重复插入
+  let newList = []
+  list.forEach(newItem => {
+    let flag = allData.find(oldItem => oldItem.enterTime === newItem.enterTime)
+    if (!flag) newList.push(newItem)
+  })
+
+  ZbModel.insertMany(newList).then(data => {
     res.send({ status: 200, data: 'ok' })
   })
 })
